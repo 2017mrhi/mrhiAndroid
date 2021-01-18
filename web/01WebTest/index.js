a4ae2a5d34d9006ce9c9daf26bb8c24571647f58
2017mrhi <37605893+2017mrhi@users.noreply.github.com>
2021-01-19 08:52:07 +0900

    Add files via upload

---
+++ web/01WebTest/index.js
@@ -0,0 +1,10 @@
+//함수 선언
+function show(){
+    alert('로그인 버튼 클릭');//다이얼로그 보여주는 함수                
+}
+
+function changeImage(){
+    //이미지를 가지고 있는 요소(img)를 찾아오기
+    var e= document.getElementById('aa');
+    e.src="./koala.jpg";
+}
